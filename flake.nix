0bf30587ef9c6b9e063f48192cb3e34c4b8ae660
Bailey <bbignall1225@gmail.com>
2025-07-27 18:34:31 -0500

    fixed shell to work regardless of config.toml

--- flake.nix
+++ flake.nix
@@ -10,13 +10,17 @@
     flake-compat.url = "https://flakehub.com/f/edolstra/flake-compat/1.tar.gz";
   };
 
-  outputs = { self, nixpkgs, fenix, ... }:
-    let pkgs = nixpkgs.legacyPackages."x86_64-linux";
-    in {
-      devShells."x86_64-linux".default = pkgs.mkShell {
-        buildInputs = [
-          (with fenix.packages."x86_64-linux";
-            with stable;
+  outputs = {
+    nixpkgs,
+    fenix,
+    ...
+  }: let
+    pkgs = nixpkgs.legacyPackages."x86_64-linux";
+  in {
+    devShells."x86_64-linux".default = pkgs.mkShell {
+      buildInputs = [
+        (with fenix.packages."x86_64-linux";
+          with stable;
             combine [
               rustc
               cargo
@@ -25,35 +29,35 @@
               rust-analyzer
               clippy
             ])
-          pkgs.pkg-config
-          pkgs.alsa-lib
-          pkgs.udev
-          pkgs.xorg.libXcursor
-          pkgs.xorg.libXi
-          pkgs.xorg.libXrandr
-          pkgs.xorg.libX11
-          pkgs.vulkan-loader
-          pkgs.libxkbcommon
-          pkgs.mold
-        ];
 
-        #make sure to add specific target in .cargo/config.toml
-        RUSTFLAGS = [
-          "-Clink-arg=-fuse-ld=${pkgs.mold}/bin/mold"
-          "-Clink-arg=-fuse-ld=lld"
-        ];
+        pkgs.clang
+        pkgs.pkg-config
+        pkgs.alsa-lib
+        pkgs.udev
+        pkgs.xorg.libXcursor
+        pkgs.xorg.libXi
+        pkgs.xorg.libXrandr
+        pkgs.xorg.libX11
+        pkgs.vulkan-loader
+        pkgs.libxkbcommon
+        pkgs.mold
+      ];
 
-        shellHook = ''
-          export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${
-            pkgs.lib.makeLibraryPath [
-              pkgs.udev
-              pkgs.alsa-lib
-              pkgs.vulkan-loader
-              pkgs.libxkbcommon
-            ]
-          }"
-          nu'';
-      };
+      #adds mold to the compile system
+      RUSTFLAGS = "-Clink-arg=-fuse-ld=${pkgs.mold}/bin/mold";
+
+      #sets clang as the linker (for mold)
+      CARGO_TARGET_X86_64_UNKNOWN_LINUX_GNU_LINKER = "clang";
+
+      #puts .cargo/ folder in the directory above
+      CARGO_HOME = "../.cargo/";
+
+      LD_LIBRARY_PATH = "$LD_LIBRARY_PATH:${pkgs.lib.makeLibraryPath [
+        pkgs.udev
+        pkgs.alsa-lib
+        pkgs.vulkan-loader
+        pkgs.libxkbcommon
+      ]}";
     };
+  };
 }
-
